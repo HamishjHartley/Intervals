355d765d21a31a739922086c3f0ce4f4531bc5a5
“HamishjHartley” <113518445+HamishjHartley@users.noreply.github.com>
2026-08-18 13:22:17 +0100

    Inital commit

---
+++ src/app/app.ts
@@ -0,0 +1,36 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {form, FormField} from '@angular/forms/signals'
+import {MatTableModule} from '@angular/material/table';
+
+interface LapData {
+  duration: number;
+  power: number
+}
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormField, MatTableModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('interval');
+
+  intervalModel = signal<LapData>({duration: 0, power: 0}); // Creating form model
+  intervalForm = form(this.intervalModel); // Creating a FieldTree
+
+  laps:LapData[] = []; // Array to store created laps
+  lapSignal = signal(this.laps);
+  columnsToDisplay = ['position', 'duration', 'power'];
+
+  addLap(event: Event) {
+    event.preventDefault()
+    const lap = this.intervalModel();
+    this.laps = [...this.laps, lap];
+
+    console.log(lap);
+  }
+
+
+}
